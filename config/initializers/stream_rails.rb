0012f1f6f3fb2b910f75fb5c7263aca98d8a5849
ian douglas <ian.douglas@iandouglas.com>
2016-09-06 15:50:49 -0600

    Updated for stream-rails 2.4, lots of new documentation added, and added some missing configs that will make it easier for new users to get up and running.

--- config/initializers/stream_rails.rb
+++ config/initializers/stream_rails.rb
@@ -1,4 +1,18 @@
 require 'stream_rails'
 
 StreamRails.configure do |config|
+
+  # you can find these details from your GetStream.io dashboard
+  config.api_key     = ''
+  config.api_secret  = ''
+  config.api_site_id = ''
+
+  config.timeout     = 30                # Optional, defaults to 3
+  config.location    = 'us-east'         # Optional, defaults to 'us-east'
+
+  # these are the example feeds in the demo app; make sure they match what you define for your application
+  config.news_feeds = {flat: 'timeline_flat', aggregated: 'timeline_aggregated'}
+  # Point to the notifications feed group providing the name, omit if you don't
+  # have a notifications feed
+  config.notification_feed = 'notification'
 end
